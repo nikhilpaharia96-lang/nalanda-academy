1ea70b470b61a16f0fd69da1c80cc3c80b08a319
Nikhil Paharia <nikhilpaharia96@gmail.com>
2026-09-08 20:57:25 +0530

    Delete nalanda-academy-website-updated/nalanda-academy directory

--- nalanda-academy-website-updated/nalanda-academy/lib/services/facultyService.ts
+++
@@ -1,9 +0,0 @@
-import { facultyMembers } from "@/lib/content/faculty";
-import { USE_MOCK_DATA, apiGet } from "@/lib/services/apiClient";
-import type { FacultyMember } from "@/lib/types";
-
-// Future: GET /api/faculty
-export async function getFaculty(): Promise<FacultyMember[]> {
-  if (USE_MOCK_DATA) return facultyMembers;
-  return apiGet<FacultyMember[]>("/api/faculty");
-}
